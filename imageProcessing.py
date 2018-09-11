0ccbd8b6280af3240566fe0bc7efbf210c91d9ad
Adam Harmanec <adam.harmanec@volny.cz>
2018-09-11 14:45:32 +0200

    added more image pipeline utilities

--- imageProcessing.py
+++ imageProcessing.py
@@ -15,6 +15,23 @@ def resizeImages(images, size):
         resizedImages.append(numpy.array(resized_image))
     return resizedImages
 
+def preprocessImages(images):
+    '''
+    Convert images to grayscale and scale their values between 1 and 2
+    :param images: list of images to be processed
+    :return: list of processed images
+    '''
+    processedImages = []
+    for image in images:
+        # convert to grayscale, YCbCr representation
+        processedImage = 0.299 * image[:, :, 0] + 0.587 * image[:, :, 1] + 0.114 * image[:, :, 2]
+
+        #scale features between 0 and 1
+        processedImage = processedImage / 255.0
+
+        processedImages.append(processedImage)
+    return processedImages
+
 
 import random
 
@@ -31,3 +48,14 @@ def unisonShuffle(a, b):
     c = list(zip(a, b))
     random.shuffle(c)
     return zip(*c)
+
+# puts all necessary steps together
+def process(images, labels, shuffle=True):
+    images = resizeImages(images, (32, 32))
+    images = preprocessImages(images)
+    if shuffle:
+        images, labels = unisonShuffle(images, labels)
+    images = numpy.array(images)
+    images = numpy.expand_dims(images, 4)
+    labels = numpy.array(labels)
+    return images, labels
