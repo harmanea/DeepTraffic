8eaee09662b960d9476f60c5b7c8ce512bb1c47f
Adam Harmanec <adam.harmanec@volny.cz>
2018-09-20 11:47:11 +0200

    Added JavaDoc

--- Android/app/src/main/java/com/harmanec/adam/deeptraffic/Classifier.java
+++ Android/app/src/main/java/com/harmanec/adam/deeptraffic/Classifier.java
@@ -87,6 +87,11 @@ public interface Classifier {
         }
     }
 
+    /**
+     * Get predictions for an image
+     * @param pixels image data
+     * @return list of predictions
+     */
     List<Recognition> recognizeImage(float[] pixels);
 
     void enableStatLogging(final boolean debug);
